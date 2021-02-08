cc7ead5cf978eefee145f3b69fe2a30d7149c7e1
frederico-campello <fredfmc33@gmail.com>
2021-02-08 18:47:09 -0300

    Requisito 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,15 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) <= 1 or len(nums) == len(set(nums)):
+        return False
+    for item in nums:
+        if type(item) == str or item < 0:
+            return False
+
+    # dica - ordenar o array nums
+    nums.sort()
+
+    for index in range(len(nums) - 1):
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
