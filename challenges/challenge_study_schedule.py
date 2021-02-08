e95282954845f971675d45e8b950c3128c7f6b58
igorgbr <igorgphotoarte@gmail.com>
2021-02-08 18:35:24 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,26 @@
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+target_time = 0
+
+
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    answer = 0
+    if not start_time:
+        return answer
+    # ------------- Primeiro Código-----------
+    # for i in range(len(end_time)):
+    #     if target_time <= end_time[i]:
+    #         if start_time[i] <= target_time:
+    #             answer += 1
+
+    # ------------- Primeira refatoração -----------
+    # for i in range(len(end_time)):
+    #     if end_time[i] >= target_time >= start_time[i]:
+    #         answer += 1
+
+    # ------------- Código final (Pythonic)-----------
+    for input, end_time in zip(start_time, end_time):
+        if input <= target_time <= end_time:
+            answer += 1
+
+    return answer
