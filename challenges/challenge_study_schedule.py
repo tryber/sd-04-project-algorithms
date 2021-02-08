822cd2e0ce00dc0d695d9fb363921bcbf6d98f74
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-08 16:41:33 -0300

    github-actions - Erros - 0

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,3 @@
-# Iniciando o projeto algorithms
-"""
-1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)
-
-Dica: Quando vou saber qual o melhor horário? Quando o contador retornado pela função for o maior.
-"""
-
 def study_schedule(start_time, end_time, target_time):
     counter_best_hour = 0
     if not start_time:
