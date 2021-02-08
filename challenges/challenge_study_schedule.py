e0c8c7afa8ce2fca7a87ec3ec8b9fa8384ba0f87
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2021-02-08 19:38:16 -0300

    req 3 done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
     studing = 0
-    for i in range(len(start_time)):
-        if end_time[i] >= target_time >= start_time[i]:
+    for start, end in zip(start_time, end_time):
+        if start >= target_time >= end:
             studing += 1
 
     return studing
