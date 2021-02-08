51fa82265fe1827d74ce73dd9d0b98c927d13051
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-08 17:02:22 -0300

    Req. 1 - study_schedule - um pouquinho mais rápido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,7 +4,8 @@ def study_schedule(start_time, end_time, target_time):
         return counter_best_hour
 
     for index in range(len(end_time)):
-        if start_time[index] <= target_time and target_time <= end_time[index]:
-            counter_best_hour += 1
+        if target_time <= end_time[index]:
+            if start_time[index] <= target_time:
+                counter_best_hour += 1
 
     return counter_best_hour
