911d2a195e1946d7cf38cab6752fdf31df49e7da
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-08 16:35:20 -0300

    Req. 1 - study_schedule - faz a busca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,9 +6,12 @@ Dica: Quando vou saber qual o melhor horário? Quando o contador retornado pela 
 """
 
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
-    count_best_hour = 0
+    counter_best_hour = 0
     if not start_time:
-        return count_best_hour
+        return counter_best_hour
 
-    
+    for index in range(len(end_time)):
+        if start_time[index] <= target_time and target_time <= end_time[index]:
+            counter_best_hour += 1
+
+    return counter_best_hour
