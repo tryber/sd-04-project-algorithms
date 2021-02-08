c5df931f2cb060ee3a8e8dbff30f52e560b68a8b
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-08 16:06:33 -0300

    Req. 1 - study_schedule - start_time_vazio

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,14 @@
 # Iniciando o projeto algorithms
+"""
+1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)
 
+Dica: Quando vou saber qual o melhor horário? Quando o contador retornado pela função for o maior.
+"""
 
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    count_best_hour = 0
+    if not start_time:
+        return count_best_hour
+
+    
