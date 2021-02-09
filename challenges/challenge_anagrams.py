e6377d42dcb897e8c82b7d38f5d22d6bfd0f71e3
igorgbr <igorgphotoarte@gmail.com>
2021-02-09 17:48:08 -0300

    req bonus

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,4 @@
 def is_anagram(first_string, second_string):
-
     if (len(first_string) == len(second_string)):
         x = [data for data in first_string]
         y = [data for data in second_string]
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
+from statistics import multimode
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums or len(nums) == 1:
+        return False
+
+    num_list = multimode([x for x in nums if isinstance(x, int) and x >= 0])
+
+    if not num_list or len(num_list) > 1:
+        return False
+
+    return num_list[0]
