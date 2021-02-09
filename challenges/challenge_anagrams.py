c974dd0622b941297169ca846385383c2a1e2337
igorgbr <igorgphotoarte@gmail.com>
2021-02-09 16:25:23 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+
+    if (len(first_string) == len(second_string)):
+        x = [data for data in first_string]
+        y = [data for data in second_string]
+        return(set(x) == set(y))
+
+    return False
