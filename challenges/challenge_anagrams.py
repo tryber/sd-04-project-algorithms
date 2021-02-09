413c6575e86a13da4579cf4863bc2f2988308a96
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-09 17:56:46 -0300

    Req. 3 - is_anagram - incompleto

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
+"""
+3 - Anagramas (Algoritmo de ordenação)
+"""
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    if len(first_string) != len(second_string):
+        return False
