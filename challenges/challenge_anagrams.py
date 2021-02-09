7363f01d3db5b461927e3f75af7ef8f3f1fd5330
frederico-campello <fredfmc33@gmail.com>
2021-02-09 17:27:26 -0300

    Requisito 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
 def is_anagram(first_string, second_string):
-    if not first_string or not second_string:
+    if len(first_string) != len(second_string):
         return False
+
+    char_qty = dict()
+
+    for i in first_string:
+        if i in char_qty:
+            char_qty[i] += 1
+        else:
+            char_qty[i] = 1
+
+    for i in second_string:
+        if i in char_qty:
+            char_qty[i] -= 1
+        else:
+            char_qty[i] = 1
+
+    for i in char_qty:
+        if char_qty[i] != 0:
+            return False
+    return True
