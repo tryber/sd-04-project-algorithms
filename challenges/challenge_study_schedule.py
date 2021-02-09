dddfde61dd62a30b46c6d6d16085d8178eb1bb64
mirellaalves <mirella.alves@gmail.com>
2021-02-08 23:04:48 -0300

    requirement 1, has to check evaluator execution time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
-    pass
+    students_online = 0
+    both_times = start_time + end_time
+
+    for index, both_time in enumerate(both_times):
+        if start_time == [] or end_time == [] or index == len(start_time):
+            return students_online
+        else:
+            pair = [both_time] + [both_times[int(index + len(both_times) / 2)]]
+            for n in range(pair[0], pair[1] + 1):
+                if n == target_time:
+                    students_online += 1
