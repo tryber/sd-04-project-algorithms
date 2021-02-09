7eaa04b987a517bb9474011f24e9297018be7293
igorgbr <igorgphotoarte@gmail.com>
2021-02-09 15:36:51 -0300

    ultima refatoracao do req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,10 +1,8 @@
 def is_palindrome_recursive(word, low=0, high=0):
-    high = len(word) - 1
-
     if word == "" or word[low] != word[high]:
         return False
 
     if len(word) == 1:
         return True
 
-    return is_palindrome_recursive(word[low + 1: high])
+    return is_palindrome_recursive(word[low + 1: len(word) - 1])
