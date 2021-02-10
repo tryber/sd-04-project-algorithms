7ece3a5a48f29032cebc95cd0b5c0c20725d99e9
Victor L <victor.lustosacp@gmail.com>
2021-02-10 09:08:46 -0300

    cc

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,7 @@ def study_schedule(start_time, end_time, target_time):
     if not start_time or not end_time or target_time == 0:
         return 0
     for index, element in enumerate(end_time):
-        if element >= target_time:  
+        if element >= target_time:
             if start_time[index] <= target_time:
                 temp += 1
     return temp
