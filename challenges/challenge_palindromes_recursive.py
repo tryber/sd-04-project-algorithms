142d59b48791d85b4987e7c8b31b7e4a73fed8fd
Victor L <victor.lustosacp@gmail.com>
2021-02-10 09:41:46 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,6 +7,6 @@ def is_palindrome_recursive(word, low, high):
     if (word[low] != word[high]):
         return False
     if (low < high + 1):
-        return is_palindrome_recursive(word, low + 1, high - 1);
+        return is_palindrome_recursive(word, low + 1, high - 1)
 
     return True
