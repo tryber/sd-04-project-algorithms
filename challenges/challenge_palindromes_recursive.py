25d5238d0953547ec904dab48795710976856571
mirellaalves <mirella.alves@gmail.com>
2021-02-09 21:15:51 -0300

    requirement 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if not word or word[low] != word[high]:
+        return False
+    else:
+        low += 1
+        high -= 1
+        if low <= high:
+            return is_palindrome_recursive(word, low, high)
+        else:
+            return True
