7e74d01710df8d43a52899d0f86d4aa18f62e8de
mirellaalves <mirella.alves@gmail.com>
2021-02-09 21:53:08 -0300

    requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,5 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if ''.join(sorted(first_string)) == ''.join(sorted(second_string)):
+        return True
+    else:
+        return False
