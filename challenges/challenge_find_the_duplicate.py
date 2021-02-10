b7ca27a15d49d414bf28f47b350a2fd2b735668c
mirellaalves <mirella.alves@gmail.com>
2021-02-09 22:24:15 -0300

    requirement 3python3

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    sorted_nums = sorted(nums)
+
+    for i, sn in enumerate(sorted_nums):
+        while i < len(sorted_nums):
+            if sorted_nums[i] == sorted_nums[i + 1]:
+                return True
+            else:
+                i += 1
+        return False
+
+
+nums = [3, 1, 2, 4, 6, 5, 7, 7, 7, 8]
+print(find_duplicate(nums))
