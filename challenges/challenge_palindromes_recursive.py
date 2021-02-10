040750dca27a8394e090cf806a77de360df662bf
Victor L <victor.lustosacp@gmail.com>
2021-02-10 09:29:10 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
+    if (high == 0) :
+        return 0
+    if (low == high): 
+        return True
+    if (word[low] != word[high]) : 
+        return False
+    if (low < high + 1): 
+        return is_palindrome_recursive(word, low + 1, high - 1); 
+  
+    return True
