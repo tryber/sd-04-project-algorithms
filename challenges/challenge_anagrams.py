ab869a14a7b95ffae94553129f8577bd2b737a89
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-02-11 19:55:28 -0300

    req 03 - complemento

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,14 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (len(first_string) < 1 or len(second_string) < 1):
+        return False
     sorted_first = sorted(first_string)
     sorted_second = sorted(second_string)
-    # print(sorted_first)
-    # print(sorted_second)
-    # print(len(sorted_first))
     output = True
     for index, letter in enumerate(sorted_first):
-        # print(sorted_first[index] != sorted_second[index])
         if sorted_first[index] != sorted_second[index]:
-            # print("inside if", sorted_first[index])
             output = False
     return output
