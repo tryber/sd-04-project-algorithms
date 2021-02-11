4ca6f3f20133d37333fd8dd0ab1a0b3388fe4bc3
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-02-11 19:03:50 -0300

    req 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+
+    if high == low:
+        return True
+    if word[high] != word[low]:
+        return False
+
+    return is_palindrome_recursive(word, low + 1, high-1)
