d6a57e0af834e1db62e0b659505aade47a3f4036
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-02-11 20:23:23 -0300

    req 04 -02

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -5,8 +5,12 @@ def find_duplicate(nums):
     # sorted_nums = nums.sort()
     sorted_nums = sorted(nums)
     # print(sorted_nums)
-    n_minus_one = sorted_nums[0]
+    n_minus_one = 0
     for num in sorted_nums:
-        if n_minus_one == num:
+        if (type(num) is not int or num < 0):
             return False
+        if n_minus_one == num:
+            return num
         n_minus_one = num
+    return False
+    
