e0098e130c59afa463bc61b4795e559d47c0998d
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-02-11 19:33:48 -0300

    req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    sorted_first = sorted(first_string)
+    sorted_second = sorted(second_string)
+    # print(sorted_first)
+    # print(sorted_second)
+    # print(len(sorted_first))
+    output = True
+    for index, letter in enumerate(sorted_first):
+        # print(sorted_first[index] != sorted_second[index])
+        if sorted_first[index] != sorted_second[index]:
+            # print("inside if", sorted_first[index])
+            output = False
+    return output
