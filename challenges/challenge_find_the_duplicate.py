63ab7eeb9d885d5ad1074e0c7c93bbe54e10a451
mirellaalves <mirella.alves@gmail.com>
2021-02-11 16:58:34 -0300

    fix requirement 4 scope

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -11,16 +11,4 @@ def find_duplicate(nums):
             i < (len(sorted_nums) - 1) and sorted_nums[i] == sorted_nums[i + 1]
         ):
             return sorted_nums[i]
-        elif i < (len(sorted_nums) - 1):
-            i += 1
-        else:
-            return False
-
-
-# nums = [3, 1, 2, 4, 6, 5, 7, 7, 8]
-# nums = [3, 1, 2, 4, 6, 5, 7, 8]
-nums = []
-# nums = ["a", "b"]
-# nums = [1]
-# nums = [-1, -1]
-print(find_duplicate(nums))
+    return False
