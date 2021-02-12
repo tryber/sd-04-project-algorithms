7641674ed73ec4a2cca1dbf1d71d89492034f679
jessica-tavares <jessicatavaraeseel@gmail.com>
2021-02-12 14:18:02 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    limit = len(word) // 2
+    if len(word) == 1:
+        return True
+    elif len(word) == 0 or word[low] != word[high]:
+        return False
+    elif low > limit:
+        return True
+    else:
+        return is_palindrome_recursive(word, low + 1, high - 1)
