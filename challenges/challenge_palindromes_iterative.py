53fc98dc4e7197cf202319318215185fdd5480ea
mirellaalves <mirella.alves@gmail.com>
2021-02-12 22:29:27 -0300

    requirement 5, refactor for less complexity and test avaliator time complexity

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,9 +1,13 @@
 def is_palindrome_iterative(word):
     if word == "":
         return False
-    for index, letter in enumerate(word):
-        if letter != word[len(word) - 1 - index]:
+
+    compare = len(word) - 1
+
+    for letter in word:
+        if letter != word[compare]:
             return False
-            if index >= len(word) // 2:
-                break
+        compare -= 1
+        if compare < len(word) / 2:
+            break
     return True
