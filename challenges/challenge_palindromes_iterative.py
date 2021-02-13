736a49b413be1edfa2fdc87d966346ecce113f8b
mirellaalves <mirella.alves@gmail.com>
2021-02-12 22:03:03 -0300

    requirement 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    for index, letter in enumerate(word):
+        if letter != word[len(word) - 1 - index]:
+            return False
+            if index >= len(word) // 2:
+                break
+    return True
