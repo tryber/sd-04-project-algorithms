ce43efcad1e0ccb99da3dab070570e8a1c6ed9cd
deboracosilveira <deboracosilveira@gmail.com>
2021-02-14 20:24:07 -0300

    Req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,5 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if sorted(first_string) == sorted(second_string):
+        return True
+    else:
+        return False
