587cd3bc96af1650daffd0e98540e37540b53a14
deboracosilveira <deboracosilveira@gmail.com>
2021-02-14 19:33:54 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if not start_time or not target_time:
+        return counter
+
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time <= end_time[index]:
+            counter += 1
+
+    return counter
