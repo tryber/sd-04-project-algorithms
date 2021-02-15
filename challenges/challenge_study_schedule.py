ebdbf9be6d2b8cf8e9d8e09596d653489bbb9fa7
Joao Soutto <joaosoutto@hotmail.com>
2021-02-15 11:05:01 -0300

    testes ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,4 +2,8 @@ def study_schedule(start_time, end_time, target_time):
     if start_time == "" or end_time == "":
         return 0
 
-    return False
+    count = 0
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time <= end_time[i]:
+            count += 1
+    return count
