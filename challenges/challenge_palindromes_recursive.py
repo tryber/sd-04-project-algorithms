325279f3267751ee063b91ee86f88973ff71ce91
deboracosilveira <deboracosilveira@gmail.com>
2021-02-15 10:16:47 -0300

    Req 2 +-

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if not word or word[low] != word[high]:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    return is_palindrome_recursive(word, low + 1, high - 1)
