c865806a63fd6399abadcd643a636338b2e9dc72
aliceaatalla <aliceatalla36@gmail.com>
2021-02-15 20:29:14 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    elif (sorted(first_string) == sorted(second_string)):
+        return True
+    else:
+        return False
