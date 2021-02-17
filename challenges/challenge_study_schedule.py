7ed2754f889dd693c35b4be7b731b3ca0819a7ef
juniomelos <juniomelos@gmail.com>
2021-02-17 11:29:36 -0300

    test_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,6 @@
 def study_schedule(start_time, end_time, target_time):
     count = 0
+    for i, number in enumerate(start_time):
+        if start_time[i] <= target_time <= end_time[i]:
+            count += 1
     return count
