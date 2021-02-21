67a69ca292ba8f10b36b5fa18815203c4077269a
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-02-21 17:56:55 -0300

    Alteração requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,9 @@
 def is_palindrome_recursive(word, low, high):
-    if high <= low:
-        return True
-
     if word == "":
         return False
+        
+    if high <= low:
+        return True
 
     if word[low] == word[high]:
         return is_palindrome_recursive(word, low + 1, high - 1)
