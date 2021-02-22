96cf630489c52f7b1624d61a20ea9effa709bb70
Thabata <dornelasthabata@gmail.com>
2021-02-22 18:20:49 -0300

    challenges

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
+# https://www.sanfoundry.com/python-program-check-string-palindrome/
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+
+    if not word:
+        return False
+    if(word == word[::-1]):
+        return True
+    else:
+        return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+
+    melhor_horario = 0
+
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time and target_time <= end_time[i]:
+            melhor_horario += 1
+
+    return melhor_horario
+
+start_time = []
+end_time = [5, 4, 3, 4, 5]
+print(study_schedule(start_time, end_time, 4))
