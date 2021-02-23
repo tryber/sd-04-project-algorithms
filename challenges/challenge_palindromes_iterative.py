06626e902455919a47f6bf99bdd36e26cda86570
danilossousa <danilosantossousa@gmail.com>
2021-02-23 12:54:14 -0300

    v1.0.1

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    cur = 0
+    last = len(word) - 1
+    palindrome = True
+    if not word:
+        return False
+    while cur < last:
+        palindrome = palindrome and word[cur] == word[last]
+        cur += 1
+        last -= 1
+    return palindrome
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    target_time_counter = 0
+    if len(start_time) > 0:
+        for i in range(len(end_time)):
+            if target_time in range(start_time[i], end_time[i] + 1):
+                target_time_counter += 1
+    return target_time_counter
