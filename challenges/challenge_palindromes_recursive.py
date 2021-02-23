e272d4940024473d8fbc5e8ba3758307eabc849b
Mateus-Moreira <ashuraw@hotmail.com>
2021-02-23 13:39:37 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if low >= high:
+        return True
+
+    if word[low] == word[high]:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+
+    return False
