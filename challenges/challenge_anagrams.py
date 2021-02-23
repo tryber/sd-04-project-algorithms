b144925dd60705fd1c2ae483d07f280808b27e35
danilossousa <danilosantossousa@gmail.com>
2021-02-23 13:01:33 -0300

    v3.0.1

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -11,7 +11,8 @@ def insertion_sort(array):
     for i in range(len(array)):
         current_value = array[i]
         current_position = i
-        while (current_position > 0
+        while (
+            current_position > 0
             and array[current_position - 1] > current_value
         ):
             array[current_position] = array[current_position - 1]
