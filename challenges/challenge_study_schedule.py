a86c47c0b7bb4ed52810f3a947b32a503bf588d9
Mateus-Moreira <ashuraw@hotmail.com>
2021-02-23 13:36:27 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    student = 0
+    for index, hour in enumerate(start_time):
+        if hour <= target_time <= end_time[index]:
+            student += 1
+    return student
