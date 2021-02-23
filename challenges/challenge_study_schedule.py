8f250815e193f350b07f54cf583270f0814eb22a
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-22 23:22:16 -0300

    completed requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,6 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    people = 0
+    for index, hour in enumerate(start_time):
+        if hour <= target_time <= end_time[index]:
+            people += 1
+    return people
