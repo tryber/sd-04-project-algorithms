fafcfc4d65c09e1de4f21c0f7892087bbaf5b93a
Capi Etheriel <barraponto@gmail.com>
2021-03-04 10:41:12 -0300

    insert_sort_solution for anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,14 +1,19 @@
 def insert_sort(iterable):
     listed = list(iterable)
+    swapping = False
 
     while swapping:
         swapping = False
         for index, item in enumerate(listed):
-            if (index + 1 < len(item)) and item < listed[index+1]:
-                listed[index], listed[index+1] = listed[index+1], listed[index]
+            if (index + 1 < len(item)) and item < listed[index + 1]:
+                listed[index], listed[index + 1] = (
+                    listed[index + 1],
+                    listed[index],
+                )
                 swapping = True
 
     return listed
+
 
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
