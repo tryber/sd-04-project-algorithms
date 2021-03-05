c509d30067d8bee80bdc792df85b2e58765d1614
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-03-05 14:38:27 -0300

    requirement 3 refactor

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
+def selection_string_sort(string):
+    str_array = [letter for letter in string]
+    for i in range(len(str_array)):
+        minimum = i
+
+        for j in range(i + 1, len(str_array)):
+            if str_array[j] < str_array[minimum]:
+                minimum = j
+
+        str_array[minimum], str_array[i] = str_array[i], str_array[minimum]
+
+    return "".join(str_array)
+
+
 def is_anagram(first_string, second_string):
-    return True if sorted(first_string) == sorted(second_string) else False
+    first_string_sorted = selection_string_sort(first_string)
+    second_string_sorted = selection_string_sort(second_string)
+    return True if first_string_sorted == second_string_sorted else False
