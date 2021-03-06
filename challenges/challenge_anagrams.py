c0c920e75c307aa3a68ebc11deb1ea0119194da5
juniomelos <juniomelos@gmail.com>
2021-03-06 18:43:02 -0300

    Alteraçao requisito 3 de ordenaçao sem a utilizaçao do sorted.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,27 @@
 def is_anagram(first_string, second_string):
-    if sorted(first_string) == sorted(second_string):
-        return True
-    return False
+
+    if (len(first_string) != len(second_string)):
+        return False
+
+    word_dic = {}
+
+    # n iterations
+    for char in first_string:
+        if word_dic.get(char):
+            word_dic[char] += 1
+        else:
+            word_dic[char] = 1
+
+    # n iterations
+    for char in second_string:
+        if word_dic.get(char):
+            word_dic[char] -= 1
+        else:
+            return False
+
+    # n iterations
+    for v in word_dic.values():
+        if v != 0:
+            return False
+
+    return True
