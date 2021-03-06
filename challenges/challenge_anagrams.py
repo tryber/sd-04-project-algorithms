24ca4063fa7168654f9df9c96f9f4acd07ecd2b7
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-03-06 19:13:56 -0300

    sort me this please 2

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,21 +1,18 @@
 def sort_me_this_please(word):
     word_listed = list(word)
-    
     i = 0
     while i < len(word_listed):
         minimum = i
-        
-        j = i +1 
+        j = i +1
         while j < len(word_listed):
             if word_listed[j] < word_listed[minimum]:
                 minimum = j
             j += 1
-        word_listed[minimum], word_listed[i] = word_listed[i], word_listed[minimum]
-        i += 1 
-                    
+        temp = word_listed[minimum]
+        word_listed[minimum] = word_listed[i]
+        word_listed[i] = temp
+        i += 1
     return word_listed
-
-
 
 def is_anagram(first_string, second_string):
     if (len(first_string) < 1 or len(second_string) < 1):
