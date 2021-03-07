15f6143309882fab03d77bf3f689feeeddc7f72c
Tiago-ers <trsilva284@gmail.com>
2021-03-07 20:14:30 -0300

    req. 1 - passando

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    count = 0
+    start = len(start_time)
+    for i in range(start):
+        if start_time[i] <= target_time <= end_time[i]:
+            count += 1
+    return count
