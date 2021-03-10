989911675e1458f0ecffb4b4ff080c5dcccc986b
henrique <hbaetaleite@gmail.com>
2021-03-10 19:29:44 -0300

    tentando finalizar

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,10 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-    # Iniciando projeto
+    if len(first_string) != len(second_string):
+        return False
+
+    for index in first_string:
+        if index not in second_string:
+            return False
+
+    return True
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    nums.sort()
+
+    for index in range(len(nums) - 1):
+        if type(nums[index]) != int or nums[index] < 0:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
