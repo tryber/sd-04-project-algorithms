6060985fe797850b597119900635e28f0f10ea0e
deboracosilveira <deboracosilveira@gmail.com>
2021-03-15 20:42:38 -0300

    Req 3 corrected

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,8 +1,21 @@
+def sort(string):
+    string_list = list(string)
+    for i in range(1, len(string_list)):
+        letter = string_list[i]
+        j = i - 1
+
+        while j >= 0 and string_list[j] > letter:
+            string_list[j + 1] = string_list[j]
+            j -= 1
+        string_list[j + 1] = letter
+    return string_list
+
+
 def is_anagram(first_string, second_string):
-    if (len(first_string) == len(second_string)):
-        first = [letter for letter in first_string]
-        second = [letter for letter in second_string]
+    if (len(first_string) != len(second_string)):
+        return False
 
-        return(set(first) == set(second))
+    first = sort(first_string)
+    second = sort(second_string)
 
-    return False
+    return first == second
