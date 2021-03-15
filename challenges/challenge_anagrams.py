972b325f69da0e676463a77772a51c24736b403a
henrique <hbaetaleite@gmail.com>
2021-03-15 16:27:49 -0300

    faltou usar as variaveis, e tinha me esquecido do set

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,6 +3,6 @@ def is_anagram(first_string, second_string):
     if len(first_string) == len(second_string):
         first_letter = [letter for letter in first_string]
         second_letter = [letter for letter in second_string]
-        return True
+        return set(first_letter) == set(second_letter)
 
     return False
