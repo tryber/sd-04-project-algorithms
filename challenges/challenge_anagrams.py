adec968d685b7ae9ac7340856f388acb6c55d1e7
juniomelos <juniomelos@gmail.com>
2021-03-15 20:47:41 -0300

    Refatorando a refatoracao do Desafio de Anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,10 +1,21 @@
+def my_sorted(word):
+    for i in range(len(word)):
+        actual_value = word[i]
+        position = i
+        while (
+            position > 0
+            and word[position - 1] > actual_value
+        ):
+            word[position] = word[position - 1]
+            position = position - 1
+        word[position] = actual_value
+    return word
+
+
 def is_anagram(first_string, second_string):
+    if len(first_string) != len(second_string):
+        return False
+    if my_sorted(list(first_string)) == my_sorted(list(second_string)):
+        return True
+    return False
 
-    counter1 = {}
-    for c in first_string.lower():
-        counter1[c] = counter1.get(c, 0) + 1
-    counter2 = {}
-    for c in second_string.lower():
-        counter2[c] = counter2.get(c, 0) + 1
-
-    return counter1 == counter2
