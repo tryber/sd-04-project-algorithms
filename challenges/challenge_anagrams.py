88a4cfb8f91bf8f63993dad8f7662763b39897b0
Filipe de Castro <filipeyoga@gmail.com>
2021-03-15 21:32:06 -0300

    fix

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,8 +3,18 @@ def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
 
-    for i in first_string:
-        if i not in second_string:
-            return False
+    if sort(first_string) == sort(second_string):
+        return True
 
-    return True
+    return False
+
+
+def sort(array):
+    for index in range(0, len(array)):
+        current_element = array[index]
+
+        while index > 0 and array[index - 1] > current_element:
+            array[index] = array[index - 1]
+            index -= 1
+
+        array[index] = current_element
