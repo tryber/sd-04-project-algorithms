88963c71797dd905460ad78cc11f177f7863cf9a
Filipe de Castro <filipeyoga@gmail.com>
2021-03-15 21:41:58 -0300

    fix

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -9,12 +9,17 @@ def is_anagram(first_string, second_string):
     return False
 
 
-def sort(array):
-    for index in range(0, len(array)):
-        current_element = array[index]
+def sort(word):
 
-        while index > 0 and array[index - 1] > current_element:
-            array[index] = array[index - 1]
+    lista = list(word)
+
+    for index in range(0, len(lista)):
+        current_element = lista[index]
+
+        while index > 0 and lista[index - 1] > current_element:
+            lista[index] = lista[index - 1]
             index -= 1
 
-        array[index] = current_element
+        lista[index] = current_element
+
+    return lista
