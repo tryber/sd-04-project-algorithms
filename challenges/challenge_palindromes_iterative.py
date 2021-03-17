eacbab908b692eeffef371ed9edcd5b4eefade40
Ilan Herbach <ilan.herbach@gmail.com>
2021-03-16 23:05:32 -0300

    Palindromes iterative and recursive done

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if (not word or word == ''):
+        return False
+    for i in range(0, len(word)):
+        if (word[len(word) - 1 - i] != word[i]):
+            return False
+    return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    if (low >= high):
+        return True
+    if (word[low] == word[high]):
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    else:
+        return False
