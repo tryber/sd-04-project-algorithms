3b4892f0ea9183713140b65b1040e9588189bb0e
Mateus Moreira Sarmento <60623845+Mateus-Moreira@users.noreply.github.com>
2021-03-16 22:00:45 -0300

    Update challenge_anagrams.py

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,7 +3,7 @@ def is_anagram(first_string, second_string):
         return False
 
     for i in second_string:
-        first_string = first_string.replace( i, "", 1)
+        first_string = first_string.replace(i, "", 1)
 
     if len(first_string) != 0:
         return False
