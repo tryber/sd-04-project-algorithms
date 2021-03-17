01a7a19485478a0af1a5a65a98a8f2d3d279b554
Mateus Moreira Sarmento <60623845+Mateus-Moreira@users.noreply.github.com>
2021-03-16 21:55:05 -0300

    Update challenge_anagrams.py

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,11 @@
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
-    
+
     for i in second_string:
-        first_string = first_string.replace(i,"", 1)
-        
+        first_string = first_string.replace( i, "", 1)
+
     if len(first_string) != 0:
-    	return False
-            
+        return False
+
     return True
