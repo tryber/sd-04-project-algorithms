4c62965ed4f98dc44d3a1762246260b43e0621c6
Ronald Lima <ronaldazevedolima@gmail.com>
2021-03-17 15:01:22 -0300

    tentando refatorar

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,24 +1,57 @@
+def diminui_complexidade(arr):
+    index = 0
+    for num in range(len(arr) - 1):
+        if arr[num] == arr[index + 1]:
+            if arr[num] < 0:
+                return False
+            else:
+                return arr[num]
+        else:
+            index += 1
+    return False
+
+
 def find_duplicate(nums):
     try:
         _ = nums[1] + 1
         order = sorted(nums)
-        index = 0
-        for num in range(len(order) - 1):
-            if order[num] == order[index + 1]:
-                # if order[num] < 0:
-                # return False
-                # else:
-                return order[num]
-            else:
-                index += 1
-        return False
+        return diminui_complexidade(order)
+        # index = 0
+        # for num in range(len(order) - 1):
+        #     if order[num] == order[index + 1]:
+        #         # if order[num] < 0:
+        #         # return False
+        #         # else:
+        #         return order[num]
+        #     else:
+        #         index += 1
+        # return False
     except TypeError:
         return False
-    # except IndexError:
-    #     return False
+    except IndexError:
+        return False
 
 
 nums = [3, 1, 2, 4, 6, 5, 7, 8]
 nums2 = [-1, -1]
 
 print(find_duplicate(nums))
+
+
+# try:
+#         _ = nums[1] + 1
+#         order = sorted(nums)
+#         index = 0
+#         for num in range(len(order) - 1):
+#             if order[num] == order[index + 1]:
+#                 # if order[num] < 0:
+#                 # return False
+#                 # else:
+#                 return order[num]
+#             else:
+#                 index += 1
+#         return False
+#     except TypeError:
+#         return False
+#     # except IndexError:
+#     #     return False
--- challenges/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-nums = [3, 1, 2, 4, 6, 5, 7, 8]
-nums2 = [-1, -1]
-
-print(find_duplicate(nums2))
