4a5aac28d5e576b16f213686cc5d4cd780716553
Ronald Lima <ronaldazevedolima@gmail.com>
2021-03-17 13:14:20 -0300

    req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,19 @@
+from collections import deque
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    fila = deque(word)
+    for _ in range(len(word) + 1):
+        # print(f"fila em cada volta {fila}")
+        if len(fila) == 0:
+            # print("chagou no len0")
+            return False
+        elif len(fila) == 1:
+            # print(f"chagou no len0 {len(fila)}")
+            return True
+        else:
+            if fila[0] != fila[-1]:
+                return False
+            else:
+                fila.pop()
+                fila.popleft()
