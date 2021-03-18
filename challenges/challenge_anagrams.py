ce7fc61c0a0fa4bbd7cdaf04cb04a33ebadd7d73
mirellaalves <mirella.alves@gmail.com>
2021-03-17 22:37:35 -0300

    req 3, try time complexity

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,84 +1,36 @@
+def sort_string(word):
+    word_letters = list(word)
+
+    for i in range(0, len(word_letters)):
+        current_letter = word_letters[i]
+        while i > 0 and word_letters[i - 1] > current_letter:
+            word_letters[i] = word_letters[i - 1]
+            i -= 1
+        word_letters[i] = current_letter
+    return word_letters
+
+
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string) \
             or first_string == '' or second_string == '':
         return False
-    x = []
-    y = []
 
-    for i_first, letter_first in enumerate(first_string):
-        for i_second, letter_second in enumerate(second_string):
-            if letter_first == letter_second \
-                    and i_first <= len(first_string) - 1:
-                x.append(letter_first)
-                second_string.replace(letter_second, '')
-                break
-            elif letter_first != letter_second \
-                    and i_second == len(second_string) - 1:
-                y.append(letter_first)
-
-    z = ''.join(x)
-    if z == first_string and y == []:
+    if sort_string(first_string) == sort_string(second_string):
         return True
     else:
         return False
 
-
-# def sort_string(word):
-#     word_letters = list(word)
-
-#     return string_to_array
+    return False
 
 
-# def is_anagram(first_string, second_string):
-#     if len(first_string) != len(second_string) \
-#             or first_string == '' or second_string == '':
-#         return False
-
-#     if sort_string(first_string) == sort_string(second_string):
-#         return True
-#     else:
-#         return False
-
-#     return False
-
-# def sort(word):
-
-#     lista = list(word)
-
-#     for index in range(0, len(lista)):
-#         current_element = lista[index]
-
-#         while index > 0 and lista[index - 1] > current_element:
-#             lista[index] = lista[index - 1]
-#             index -= 1
-
-#         lista[index] = current_element
-
-#     return lista
-
-
-# def is_anagram(first_string, second_string):
-
-#     if first_string is None or second_string is None:
-#         return False
-
-#     if len(first_string) != len(second_string):
-#         return False
-
-#     if sort(first_string) == sort(second_string):
-#         return True
-
-#     return False
-
-
-first_string = "pedra"
-second_string = "perdaaa"
+# first_string = "pedra"
+# second_string = "perdaaa"
 
 # first_string = "pedra"
 # second_string = "pedro"
 
-# first_string = "pedra"
-# second_string = "perda"
+first_string = "pedra"
+second_string = "perda"
 
 # first_string = ""
 # second_string = "perda"
@@ -88,4 +40,4 @@ second_string = "perdaaa"
 
 # print(sort_string(first_string))
 # print(sort_string(second_string))
-print(is_anagram(first_string, second_string))
+# print(is_anagram(first_string, second_string))
