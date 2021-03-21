18fadfedd0b4a95475672ebf5f36509982a276f5
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-03-21 18:51:32 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,6 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    for number in enumerate(start_time):
+        if start_time[number] <= target_time <= end_time[number]:
+            count += 1
+    return count
