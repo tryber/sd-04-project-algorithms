c91dee041ce5bda385cb79c72b68a6fcc69c5608
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-03-21 19:01:24 -0300

    fix return

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,6 @@
 def is_palindrome_recursive(word, low, high):
     if word == "" or word[low] != word[high]:
-            return False
+        return False
 
     if high == low:
         return True
