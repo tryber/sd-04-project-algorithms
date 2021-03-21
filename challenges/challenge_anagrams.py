a521fb7f506ad72cb58944edc6b274bbbb2a4ab7
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-03-21 19:42:54 -0300

    teste anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+        def qsort(string):
+            inlist = list(string)
+        if inlist == []:
+            return []
+        else:
+            pivot = inlist[0]
+            lesser = qsort([x for x in inlist[1:] if x < pivot])
+            greater = qsort([x for x in inlist[1:] if x >= pivot])
+            return lesser + [pivot] + greater
+
+    primeira = ''.join(qsort(first_string))
+    segunda = ''.join(qsort(second_string))
+
+    return primeira == segunda
