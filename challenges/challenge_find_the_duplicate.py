8a4990fc64213bc3e08183bc7ff7ee62481b9cd4
danielPantalena <pantalenadaniel@gmail.com>
2021-03-21 20:56:04 -0300

    Refactor find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,8 +1,8 @@
 def find_duplicate(numbers):
     for index, out_number in enumerate(numbers):
-        for in_number in numbers[index+1:]:
+        for in_number in numbers[index + 1:]:
             if type(out_number) is str or out_number < 0:
-                return False 
+                return False
             if out_number == in_number:
                 return out_number
     return False
