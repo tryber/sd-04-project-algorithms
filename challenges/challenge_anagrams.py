74119aa8dabe215d6a3337ad38924e76264e51d1
danielPantalena <pantalenadaniel@gmail.com>
2021-03-21 19:45:51 -0300

    Done anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,8 +2,6 @@ def merge_by_sort(first, second):
     merged = ''
     first_i, second_i = 0, 0
     while first_i < len(first) and second_i < len(second):
-        print('first_i', first_i)
-        print('second_i', second_i)
         if first[first_i] <= second[second_i]:
             merged = merged + first[first_i]
             first_i += 1
@@ -11,20 +9,22 @@ def merge_by_sort(first, second):
             merged = merged + second[second_i]
             second_i += 1
     if first_i >= len(first):
-        print('inside if')
         for index in range(second_i, len(second)):
             merged = merged + second[index]
     else:
-        print('inside else')
         for index in range(first_i, len(first)):
             merged = merged + first[index]
 
     return merged
 
-print(merge_by_sort('bbc', 'aaz'))
 
 def sort_with_merge(string):
-    pass
+    if len(string) <= 1:
+        return string
+    middle_index = len(string) // 2
+    first_part = sort_with_merge(string[:middle_index])
+    second_part = sort_with_merge(string[middle_index:])
+    return merge_by_sort(first_part, second_part)
 
 
 def is_anagram(first_string, second_string):
