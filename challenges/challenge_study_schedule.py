1a0c2a51e5bb5e738a69abc724dcec33e006e14f
danielPantalena <pantalenadaniel@gmail.com>
2021-03-21 18:16:58 -0300

    Refactor if statement at study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,5 +2,6 @@ def study_schedule(start_time, end_time, target_time):
     total_of_students_at_target_time = 0
     for index, student_start_time in enumerate(start_time):
         is_online = student_start_time <= target_time <= end_time[index]
-        if(is_online): total_of_students_at_target_time += 1
+        if(is_online):
+            total_of_students_at_target_time += 1
     return total_of_students_at_target_time
