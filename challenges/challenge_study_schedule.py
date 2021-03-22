44ba6eff15216e0ea67de65b983e558cedae786c
JorgeHSOsiro <jojimurakabe@gmail.com>
2021-03-22 19:18:27 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if start_time == [] or target_time == 0:
+        return 0
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time <= end_time[index]:
+            counter += 1
+    return counter
