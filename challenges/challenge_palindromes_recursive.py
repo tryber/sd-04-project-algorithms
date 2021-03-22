1a846644d9bebf0f8af584a5f0b3f77c196f1cc7
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-03-22 20:44:39 -0300

    cc

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,14 +1,11 @@
 def is_palindrome_recursive(word, low, high):
-    
+
     if word == "":
         return False
-
     if high == low:
         return True
-
     if word[low] == word[high]:
         return is_palindrome_recursive(word, low + 1, high - 1)
-
     return False
 
-#https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
+# https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
