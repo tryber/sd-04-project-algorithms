af8ef82fe21329ecd5d128bc6d4ae52aba6062e3
caiolmf <caio.lmf@gmail.com>
2021-03-21 22:20:27 -0300

    do challange 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if high == -1:
+        return False
+
+    if low == high:
+        return True
+    
+    if word[low] != word[high]:
+        return False
+
+    if low < high+1 :
+        return is_palindrome_recursive(word, low + 1, high - 1)
+
+    return True
