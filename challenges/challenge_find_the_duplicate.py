4704c1c28407e65520789633138ce070e3f9652b
djonata-dias <diasdjonata@gmail.com>
2021-03-22 08:28:59 -0300

    criei a função que acha duplicatas na lista

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,7 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for x in nums:
+        if type(x) is  str or x < 0:
+            return False  
+        if nums.count(x) > 1:
+            return x
+    return False
