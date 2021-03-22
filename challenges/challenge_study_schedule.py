a771c331767ed69f8ec0af34a39c0fe9c27546bd
caiolmf <caio.lmf@gmail.com>
2021-03-21 21:46:26 -0300

    do chanllange 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    on_time = [
+        i for i, time in enumerate(start_time)
+        if time <= target_time <= end_time[i]
+    ]
+
+    return len(on_time)
