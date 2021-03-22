d47fb2d7eeeca3e3da210ac7b4f31a3f2a06bd56
djonata-dias <diasdjonata@gmail.com>
2021-03-21 23:03:24 -0300

    adicionando recursividade a função do palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,5 +2,9 @@ def is_palindrome_recursive(word, low, high):
 
     if (word == ''):
         return False
+    if (word[low] != word[high]):
+        return False
+    if (low < high + 1):
+        return is_palindrome_recursive(word, low + 1, high - 1)
 
     return True
