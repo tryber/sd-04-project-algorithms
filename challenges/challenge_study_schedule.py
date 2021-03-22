47ce6c7acdf4d2020dc6fe7cce105e9a4bb2e5aa
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-03-22 20:01:58 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Iniciando projeto """
+    count = 0
+
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time <= end_time[i]:
+            count += 1
+    return count
