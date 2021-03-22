dc1f35fcbf8e97a11a3111afef06cf13f700a5a8
leandromarten1 <leandromarten1@gmail.com>
2021-03-22 10:56:03 -0300

    req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    order = sorted(nums)
+    for i, number in enumerate(order):
+        # print(number, order[i])
+        # compara o numero com o próximo da lista
+        if number == order[i + 1]:
+            return number
+    return False
+
+
+# print(find_duplicate([1, 3, 4, 2, 2]))
+# print(find_duplicate([7, 3, 4, 4, 2, 1, 5, 3]))
