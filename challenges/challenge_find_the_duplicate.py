4722e9d58b4eacfcdbc690471248f729c801bfbf
caiolmf <caio.lmf@gmail.com>
2021-03-22 22:56:56 -0300

    lint

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,13 +2,11 @@ def find_duplicate(nums):
     sorted_nums = sorted(nums)
     size = len(nums)
 
-    if size <= 1 :
+    if size <= 1 or isinstance(sorted_nums[0], str):
         return False
 
     for i in range(size - 1):
         actual_number = sorted_nums[i]
-        if isinstance(actual_number, str):
-            return False
         if actual_number < 0:
             break
         if actual_number == sorted_nums[i + 1]:
