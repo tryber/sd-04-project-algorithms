3134c2e217913be345a79092be3090ae9b9c8c6b
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-03-22 21:43:49 -0300

    bonus2

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,21 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+
+    if (not word or word == ''):
+        return False
+
+    # Converta o argumento em uma string minúscula
+    char = str(word).lower()
+    size = len(char)
+    
+    # Definindo os índices para iteração
+    inicio = 0    
+    fim = size - 1
+    
+    for i in char:
+        if char[inicio] == char[fim]: # Verificando o primeiro e o último caracteres no argumento
+            inicio = inicio + 1   # Aumentando o valor do índice em 1
+            fim = fim - 1 # Reduzindo o valor do índice em 1, porque está acessando a string por trás
+            True
+        else:
+            False
+    return True
