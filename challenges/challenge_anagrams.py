a463fdb47ce69bde925f1a2089f956ec26295908
caiolmf <caio.lmf@gmail.com>
2021-03-22 21:49:44 -0300

    do challenge 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    if(sort_word(first_string.replace(" ", "")) == sort_word(second_string.replace(" ", ""))):
+        return True
+
+    return False
+
+def sort_word(word):
+    word_letters = list(word)
+    for i in range(len(word)-1):
+        temp = word_letters[i]
+        for n in range(i, len(word) - 1):
+            if word_letters[n + 1] < temp:
+                word_letters[i] = word_letters[n + 1]
+                word_letters[n + 1] = temp
+                temp = word_letters[i]
+    return word_letters
