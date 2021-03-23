7bc814acc701a6d9f2bc0770cf8e88b1ddf0310f
caiolmf <caio.lmf@gmail.com>
2021-03-22 22:40:57 -0300

    do challenge 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,17 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    sorted_nums = sorted(nums)
+    size = len(nums)
+
+    if size <= 1 :
+        return False
+
+    for i in range(size - 1):
+        actual_number = sorted_nums[i]
+        if isinstance(actual_number, str):
+            return False
+        if actual_number < 0:
+            break
+        if actual_number == sorted_nums[i + 1]:
+            return actual_number
+
+    return False
