6cc39f66f5f291a8683d29bbb165ca8f14d6c90b
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-03-22 21:18:07 -0300

    req2, req3 e bonus1

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+
+    nums.sort()
+
+    for x in range(1, len(nums)):
+        if type(nums[x]) != int or nums[x] < 0:
+            return False
+        if(nums[x] == nums[x-1]):
+            return nums[x]
+
+    return False
+# https://stackoverflow.com/questions/40167364/find-the-duplicate-number
