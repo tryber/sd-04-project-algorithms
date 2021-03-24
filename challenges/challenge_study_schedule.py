aa0732a7f7164555040941e0fd09abfd2f1e3906
ribaslucas <lucas@mundoribas.com.br>
2021-03-24 13:24:08 -0300

    Ribaslucas

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    for index, time in enumerate(start_time):
+        if time <= target_time <= end_time[index]:
+            count += 1
+
+    return count
