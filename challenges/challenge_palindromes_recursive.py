6cb88afb089332e3de669b28c50ad09457c9ec04
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-03-24 12:39:03 -0300

    palindromes recursive and iterative

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,5 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if low >= high:
+        return len(word) > 0
+    line_was_too_long = is_palindrome_recursive(word, low + 1, high - 1)
+    return word[low] == word[high] and line_was_too_long
