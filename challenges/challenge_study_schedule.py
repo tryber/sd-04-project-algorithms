b3b0b4bd7aa13ee2550fa9c9858ea3f48e6d5dbf
gabrielukke <gabriellukk@outlook.com>
2021-03-24 18:45:28 -0300

    Req 1 - All tests

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+target_time = 5
+
+
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if start_time is None or target_time is None:
+        return counter
+    for x in range(len(start_time)):
+        if start_time[x] <= target_time <= end_time[x]:
+            counter += 1
+    return counter
