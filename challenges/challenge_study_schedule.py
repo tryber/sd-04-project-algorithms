c1dd858af747e38af7bda70043c5d78f75955fa7
orlando-messias <programadorlando@gmail.com>
2021-03-23 21:53:01 -0300

    fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
     studing = 0
-    for start, end in zip(start_time, end_time):
-        if start >= target_time >= end:
+    for i in range(len(start_time)):
+        if end_time[i] >= target_time >= start_time[i]:
             studing += 1
 
     return studing
