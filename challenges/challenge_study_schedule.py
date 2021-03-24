24084a0023fc1dc9cc5f6d04270d4b32a4097bd7
orlando-messias <programadorlando@gmail.com>
2021-03-23 21:15:37 -0300

    req 01, 02 and 03

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+   
+    for start, end in zip(start_time, end_time):
+        if start >= target_time >= end:
+            studing += 1
+
+    return studing
