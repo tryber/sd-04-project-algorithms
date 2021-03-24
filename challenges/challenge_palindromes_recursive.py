73f8f149f7098985bb1ac7ec5ed56e49703eda84
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-03-24 17:35:21 -0300

    req 2 recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -6,7 +6,6 @@ def is_palindrome_recursive(word, low=0, high=0):
     if word == "" or word[low] != word[high]:
         return False
 
-
     return is_palindrome_recursive(word[low + 1: len(word) - 1])
 
     # usando a recursividade os campos, chamamos o a função novamente em loop
