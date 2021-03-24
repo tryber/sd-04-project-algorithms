ea908b792cbdf8b6577aaf7f1e329f641d43fd88
pedrotpo <pedrotpoliveira@gmail.com>
2021-03-24 20:27:41 -0300

    Req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,15 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    sorted_nums = sorted(nums)
+    size = len(nums)
+
+    if size <= 1 or isinstance(sorted_nums[0], str):
+        return False
+
+    for i in range(size - 1):
+        actual_number = sorted_nums[i]
+        if actual_number < 0:
+            break
+        if actual_number == sorted_nums[i + 1]:
+            return actual_number
+
+    return False
