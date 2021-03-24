d20dfea110ab6fdd1339a57d4fbec3c943d45268
orlando-messias <programadorlando@gmail.com>
2021-03-23 21:23:13 -0300

    fix

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,5 @@
 def is_palindrome_recursive(word, low, high):
-   
+
     if low >= high:
         return len(word) > 0
         line_was_too_long = is_palindrome_recursive(word, low + 1, high - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,5 @@
 def study_schedule(start_time, end_time, target_time):
-   
+    studing = 0
     for start, end in zip(start_time, end_time):
         if start >= target_time >= end:
             studing += 1
