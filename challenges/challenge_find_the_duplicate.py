db8544d07c4b74a9998f53ea6a8c0c3894fd1d4b
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-03-24 12:48:14 -0300

    find duplicate done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    length = len(nums)
+    if length <= 1 or isinstance(nums[0], str):
+        return False
+    sorted_nums = sorted(nums)
+    for i in range(length - 1):
+        if sorted_nums[i] <= 0:
+            break
+        if sorted_nums[i] == sorted_nums[i + 1]:
+            return sorted_nums[i]
+    return False
