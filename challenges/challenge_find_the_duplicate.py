fae9d91a3f3612dd234fb9607a791bc64bb7c822
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-03-24 23:34:24 -0300

    5, 4 in this order

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    num_checados = set()
+    prim_duplicado = False
+
+    for num in nums:
+        if num < 0:
+            return False
+        if num in num_checados:
+            prim_duplicado = num
+            break
+        num_checados.add(num)
+
+    return prim_duplicado
