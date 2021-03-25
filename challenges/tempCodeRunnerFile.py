8b627b991ffd1397bfd78b2ea9e6645eed81b07b
gabrielukke <gabriellukk@outlook.com>
2021-03-25 20:44:57 -0300

    Req 5 - Valeu W3Schools

--- challenges/tempCodeRunnerFile.py
+++ challenges/tempCodeRunnerFile.py
@@ -1,13 +1,4 @@
-def find_duplicate(nums):
-    if nums is None:
-        return False
-    if type(nums) is str:
-        return False
-    
-    for x in nums:
-        if x == x:
-            return x
-        else:
-            return False
 
-print(find_duplicate('a'))
+test = 'teste'
+
+print(test[-1::])
