23c3a30f29b906068aba0cb38e23752dd76e92b9
matheusysd <matheusyurisd@gmail.com>
2021-03-25 10:35:34 -0300

    Find the duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    ordered_nums = sorted(nums)
+    if len(nums) > 1:
+        for i, number in enumerate(ordered_nums):
+            if type(number) != int or number < int(0):
+                return False
+            if number == ordered_nums[i - 1]:
+                return number
+    return False
