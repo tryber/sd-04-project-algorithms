019e701ddd58d7a87c118e9eff637c6f716b2cb2
richardwellerson <richardwellerson@gmail.com>
2021-03-25 20:55:56 -0300

    Algorithms: Jovem emocionado se esquece de corrigir vírgula espaçada acidentalmente

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,5 +2,5 @@ def is_palindrome_recursive(word, low, high):
     if(word == '' or word[low] != word[high]):
         return False
     if(low < high + 1):
-        return is_palindrome_recursive(word, low + 1 , high - 1)
+        return is_palindrome_recursive(word, low + 1, high - 1)
     return True
