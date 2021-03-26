87cfe9c9d8117af5ce1e003fe3146afdf80ffab4
Ilan Herbach <ilan.herbach@gmail.com>
2021-03-26 18:56:44 -0300

    Blank lines expected

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -13,12 +13,16 @@ def is_anagram(first_string, second_string):
     for index in range(len(a)):
         if a[index] not in b:
             return False
+
+
 def merge_sort(array):
     if len(array) <= 1:
         return array
     mid = len(array) // 2
     left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
     return merge(left, right, array.copy())
+
+
 def merge(left, right, merged):
     left_cursor, right_cursor = 0, 0
     while left_cursor < len(left) and right_cursor < len(right):
