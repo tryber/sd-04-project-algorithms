5818a47f084b64ede4cc303fefbe574b4d05beb1
richardwellerson <richardwellerson@gmail.com>
2021-03-25 21:03:47 -0300

    Algorithms: Feito.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if start_time is None or end_time is None:
+        return 0
+    for x in range(len(start_time)):
+        if start_time[x] <= target_time <= end_time[x]:
+            counter += 1
+    return counter
