72df76ee076a3ad40fd31076e1baa14d65015d92
Luma Arruda <luma.oarruda@gmail.com>
2021-03-26 01:06:13 -0300

    req 4 - return

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,4 +5,7 @@ def is_palindrome_recursive(word, low, high):
     if len(word) == 1:
         return True
 
-    return is_palindrome_recursive(word, low + 1, high - 1)
+    if low < high+1:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+
+    return True
