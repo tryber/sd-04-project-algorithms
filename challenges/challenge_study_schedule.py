3d2243e50ccacf708b47b1705ce96646f47a0b0c
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-03-27 00:05:38 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    people = 0
+
+    if not start_time or not target_time:
+        return people
+
+    for i, current_start_time in enumerate(start_time):
+        if end_time[i] >= target_time >= current_start_time:
+            people += 1
+
+    return people
