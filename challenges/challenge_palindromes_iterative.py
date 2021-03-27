0ecf0a53fa9aa9a09bdae1e81ad6c7f03e01e58b
fogo5000 <lucaslucas2008@gmail.com>
2021-03-26 23:53:56 -0300

    Requisition 5 done

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,13 @@
 def is_palindrome_iterative(word):
-    
-
-
-
-
-palavra1 = "ANA"
-palavra1 = "REVIVER"
-palavra1 = "COXINHA"
-palavra1 = "AGUA"
-print(is_palindrome_iterative)
+    if not word:
+        return False
+    solution = True
+    word_end = len(word) - 1
+    word_begin = 0
+    while word_end > word_begin:
+        if not word[word_begin] == word[word_end]:
+            solution = False
+            break
+        word_begin += 1
+        word_end -= 1
+    return solution
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,9 @@
 def is_palindrome_recursive(word, low, high):
-    if not word: return False
+    if not word:
+        return False
     if low < high and word[low] == word[high]:
         return is_palindrome_recursive(word, low + 1, high - 1)
-    elif low >= high: return True
-    else: return False
+    elif low >= high:
+        return True
+    else:
+        return False
